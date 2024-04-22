49f1435c6e3ce3cdf7f997fd0373023374f648f4
doanhuuloc <doanhuuloc101102@gmail.com>
2024-04-22 12:44:28 +0700

    fix10

---
+++ src/views/js/home.js
@@ -0,0 +1,52 @@
+fetch("http://localhost:3000/api/getalllaptop", {
+    headers: {
+        "Content-Type": "application/json",
+    },
+    method: "GET",
+}).then(data => {
+    return data.json()
+})
+    .then(data => {
+        let listLaptopHTML = ""
+        data.forEach(laptop => {
+            if (laptop.id <= 20) {
+
+                listLaptopHTML += `
+                 <div class="laptop">
+                    <div class="laptop-img">
+                        <img src="https://ik.imagekit.io/2b6hnescv/`+ laptop.img + `" alt="">
+                    </div>
+                    <div>
+                        <p class="laptop-title">` + laptop.name + `</p>
+                    </div>
+                    <div class="laptop-price">`+ laptop.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' }) + `</div>
+                    <div class="buy-laptop" onclick="window.location.href ='purchase?laptopid=`+ laptop.id + `'">
+                        <p>mua</p>
+                    </div>
+                </div>`
+            }
+        })
+
+        document.getElementById("listLaptop").innerHTML = listLaptopHTML
+
+    }).catch(err => {
+        console.log(err);
+    })
+
+var counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) { counter = 1; }
+}, 5000);
+
+function serachLaptop() {
+
+    let search = document.getElementById('search').value
+    window.location.href = "search?search=" + search
+}
+
+document.getElementById('form').addEventListener('submit', async function (event) {
+    event.preventDefault()
+    serachLaptop()
+})
