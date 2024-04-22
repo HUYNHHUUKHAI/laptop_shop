b3c7ff7fbf5acb866ec7ecf0573f9d58176475d3
doanhuuloc <doanhuuloc101102@gmail.com>
2024-04-22 18:15:54 +0700

    fix 23

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const port = 3000
-const router = require('./src/router/index')
-const initDB = require('./src/db/initDB')
-app.use(express.static(path.join(__dirname, 'src/views')));
-app.use(express.urlencoded())
-app.use(express.json())
-router(app)
-initDB()
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// const express = require('express')
+// const app = express()
+// const path = require('path')
+// const port = 3000
+// const router = require('./src/router/index')
+// const initDB = require('./src/db/initDB')
+// app.use(express.static(path.join(__dirname, 'src/views')));
+// app.use(express.urlencoded())
+// app.use(express.json())
+// router(app)
+// initDB()
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+app.get("/", async (req, res, next) => {
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+    });
+});
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
