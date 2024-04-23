e80186189cce48eb173e55e686f2e7fcd3a9e96a
doanhuuloc <doanhuuloc101102@gmail.com>
2024-04-23 19:42:19 +0700

    2

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const initDB = require('./src/db/initDB')
 app.use(express.static(path.join(__dirname, 'src/views')));
 app.use(express.urlencoded())
 app.use(express.json())
-router(app)
+app.get("/", async (req, res, next) => {
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+    });
+});
+
+const homeController = require("./src/controller/homeController")
+app.get('/api/getalllaptop', homeController.getAllLaptop)
+
 initDB()
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
